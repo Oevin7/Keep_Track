7f479c658a065ee887a80387371ac9c58c372515
Oevin7 <131039022+Oevin7@users.noreply.github.com>
2023-08-06 22:41:31 -0400

    Succesfully redisgned a few UI elements. I have to add functionality back to the buttons.

--- Todo_List.py
+++ Todo_List.py
@@ -1,6 +1,4 @@
 import customtkinter as ctk
-from Dependencies import Frame_Gen as Frm
-from Dependencies import Click_Handling as Ch
 import enum
 
 
@@ -29,6 +27,7 @@ class Todo:
 
         ctk.set_default_color_theme("dark-blue")
         ctk.set_appearance_mode("dark-blue")
+        self.font = "Alice Yotsuba Inc."
 
         # Booleans to control UI
         self.task_process = Process.BLANK
@@ -39,71 +38,34 @@ class Todo:
         self.tdo_lst_del = {}
         self.count = 0
 
-        # Frame for the entries and buttons
-        self.etr_frm = Frm.frame_gen(self.window)
+        self.main_frame = ctk.CTkFrame(self.window, height=700, width=500)
 
-        # Entry button to add task
-        self.etr_add_btn = ctk.CTkButton(self.etr_frm, text="Add", width=10, height=28, command=self.add_task)
-        # enter_clicked = Ch.enter_click(etr=self.etr_add_btn, )
+        self.btn_frame = ctk.CTkFrame(self.main_frame, height=120)
+        self.btn_frame.place(x=0)
 
-        # Entry button to delete task
-        self.etr_del_btn = ctk.CTkButton(self.etr_frm, text="Delete", width=10, height=28,
-                                         command=self.delete_task)
+        self.add_btn = ctk.CTkButton(self.btn_frame, text="Add", font=(self.font, 16))
+        self.add_btn.place(x=30, y=15)
 
-        self.etr_man_btn = ctk.CTkButton(self.etr_frm, text="Manage", width=10, height=28)
+        self.del_btn = ctk.CTkButton(self.btn_frame, text="Delete", font=(self.font, 16))
+        self.del_btn.place(x=30, y=45)
 
-        # The entry itself
-        self.add_prompt = Ch.CstEntry(master=self.window)
+        self.man_btn = ctk.CTkButton(self.btn_frame, text="Manage", font=(self.font, 16))
+        self.man_btn.place(x=30, y=75)
 
-        # Delete Prompt for deleting tasks
-        self.delete_prompt = Ch.CstEntry(master=self.window)
+        self.lst_frame = ctk.CTkFrame(self.main_frame)
+        self.lst_frame.place(x=250)
 
-        self.manage_prompt = Ch.CstEntry(master=self.window)
-
-        # The label that displays the tasks
-        self.labl_title = ctk.CTkLabel(self.window, text="Uncompleted Tasks", font=("Sunny Spells Basic", 50))
-        self.labl_title.pack(padx=20, pady=20)
-
-        self.lbl_frame = ctk.CTkFrame(self.window, border_width=2, width=500, height=200)
-        self.lbl_frame["relief"] = "sunken"
-        self.lbl_frame.pack(padx=10, pady=10)
-
-        self.timed_lbl_frame = ctk.CTkFrame(self.window)
-        self.timed_lbl = ctk.CTkLabel(self.timed_lbl_frame, font=("Sunny Spells Basic", 18))
-
-        self.labl = ctk.CTkLabel(self.lbl_frame, text="", font=("Sunny Spells Basic", 20))
-        self.labl.pack()
-
-        # button frame and buttons
-        self.btnfrm = Frm.frame_gen(self.window)
-
-        self.btn_add = ctk.CTkButton(self.btnfrm, height=35, width=150, text="Add Task",
-                                     font=("Sunny Spells Basic", 25),
-                                     command=self.add_task_prompt)
-        self.btn_add.grid(row=0, column=0, padx=5, pady=5)
-
-        self.btn_del = ctk.CTkButton(self.btnfrm, height=35, width=150, text="Delete Task",
-                                     font=("Sunny Spells Basic", 25),
-                                     command=self.delete_task_prompt)
-        self.btn_del.grid(row=0, column=1, padx=5, pady=5)
-
-        self.btn_manage = ctk.CTkButton(self.btnfrm, height=35, width=150, text="Manage Tasks",
-                                        font=("Sunny Spells Basic", 25), command=self.manage_task_prompt)
-        self.btn_manage.grid(row=0, column=2, padx=5, pady=5)
-
-        self.btnfrm.pack(pady=10)
-
-        # self.ui_settings.pack(side=ctk.BOTTOM)
+        self.main_frame.pack()
 
     # Displays the prompt to add tasks
     def add_task_prompt(self):
         if self.task_process != Process.ADD and not self.other_process:
-                self.etr_frm.pack()
-                self.add_prompt.pack()
-                self.etr_add_btn.pack(side=ctk.RIGHT)
-                self.add_prompt.enter_bind(self.add_task)
-                self.task_process = Process.ADD
-                self.other_process = True
+            self.etr_frm.pack()
+            self.add_prompt.pack()
+            self.etr_add_btn.pack(side=ctk.RIGHT)
+            self.add_prompt.enter_bind(self.add_task)
+            self.task_process = Process.ADD
+            self.other_process = True
 
     def dict_count_add(self):
         self.count += 1
@@ -132,11 +94,11 @@ class Todo:
     # Deletes the task the user prompts
     def delete_task_prompt(self):
         if self.task_process != Process.DEL and not self.other_process:
-                self.etr_frm.pack()
-                self.delete_prompt.pack()
-                self.etr_del_btn.pack()
-                self.task_process = Process.DEL
-                self.other_process = True
+            self.etr_frm.pack()
+            self.delete_prompt.pack()
+            self.etr_del_btn.pack()
+            self.task_process = Process.DEL
+            self.other_process = True
 
     def delete_task(self):
         task_to_delete = None
@@ -161,11 +123,11 @@ class Todo:
 
     def manage_task_prompt(self):
         if self.task_process != Process.MAN and not self.other_process:
-                self.etr_frm.pack()
-                self.manage_prompt.pack()
-                self.etr_man_btn.pack()
-                self.task_process = Process.MAN
-                self.other_process = True
+            self.etr_frm.pack()
+            self.manage_prompt.pack()
+            self.etr_man_btn.pack()
+            self.task_process = Process.MAN
+            self.other_process = True
 
     def show_message(self, text):
         self.timed_lbl.configure(text="Task deleted: " + text)
@@ -180,7 +142,7 @@ class Todo:
 if __name__ == "__main__":
     window = ctk.CTk()
     window.title("Todo List")
-    window.geometry("750x500")
+    window.geometry("500x700")
 
     Todo(window)
 
